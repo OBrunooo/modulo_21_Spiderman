88b1129e7014b62fc7d642263236a7c62a461b06
Bruno Gomes <brunogmr07@gmail.com>
2024-01-29 22:42:13 -0300

    commit final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,47 @@
 $(document).ready(function () {
-    $("#button--atravesdoaranhaverso").click(function(){
-        $(this).addClass("filmes__tabs__button--is--active");
-        $("#button--noaranhaverso").removeClass("filmes__tabs__button--is--active");
-        $("#sinopse--noarahaverso").slideUp("slow", function() {
-            $("#sinopse--noarahaverso").removeClass("filmes__sinopse--is--active");
-            $("#sinopse--atravesdoaranhaverso").slideDown("slow");
+    function exibirElemento(botaoID, elementoID) {
+        if ($(elementoID).is(":visible")) {
+            return; 
+        }
+
+        $(botaoID).addClass("filmes__tabs__button--is--active");
+
+        $(".filmes__tabs__button").not(botaoID).removeClass("filmes__tabs__button--is--active");
+
+        $(".filmes__sinopse").not(elementoID).slideUp("slow", function(){
+            $(this).removeClass("filmes__sinopse--is--active");
+            
+            $(elementoID)
+                .css({display: "none", opacity: 0})
+                .delay(100)
+                .slideDown("slow", function() {
+                    $(this)
+                        .css({display: "flex"})
+                        .animate({ opacity: 1 }, "slow")
+                        .addClass("filmes__sinopse--is--active")
+                        .find(".filmes__sinopse__item__video, p")
+                        .fadeIn("slow");
+                });
         });
+    }
+
+    $("#button--atravesdoaranhaverso").click(function(){
+        exibirElemento(this, "#sinopse--atravesdoaranhaverso");
     });
+
+    $("#button--noaranhaverso").click(function(){
+        exibirElemento(this, "#sinopse--noarahaverso");
+    });
+
+    $(".header__links__item").click(function(event) {
+        event.preventDefault();
+    
+        var targetSection = $($(this).attr("href")); 
+        var offsetTop = targetSection.offset().top - 50; 
+    
+        $("html, body").animate({
+            scrollTop: offsetTop
+        }, 800);
+    });
+    
 });
