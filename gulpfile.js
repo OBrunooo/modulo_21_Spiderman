34103ef4862ce618823586407f39d4efb5470020
Bruno Gomes <brunogmr07@gmail.com>
2024-02-25 20:33:49 -0300

    ajustando package.jason e gulp.file

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,20 @@ const gulp = require ("gulp");
 const sass = require ("gulp-sass")(require("sass"));
 const uglify = require ("gulp-uglify");
 
+
+function scripts(){
+    return gulp.src("./src/scripts/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"))
+}
+
 function styles() {
-    console.log("compilando sass")
     return gulp.src("./src/styles/*.scss")
     .pipe(sass({outputStyle:"compressed"}))
     .pipe(gulp.dest("./dist/css"))
 }
 
-exports.default = gulp.parallel(styles);
+exports.default = gulp.parallel(styles, scripts);
 
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
